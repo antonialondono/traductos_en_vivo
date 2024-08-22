fa748c179852b17e2cf11476f6d91bd55155c68c
antonialondono <alondono2@eafit.edu.co>
2024-08-22 10:51:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,37 +34,6 @@ if img_file_buffer is not None:
     text=pytesseract.image_to_string(img_rgb)
     st.write(text) 
 
- recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
-
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
-    try:
-        os.mkdir("temp")
-    except:
-        pass
-    st.title("Texto a Audio")
-    translator = Translator()
     
     text = str(result.get("GET_TEXT"))
     in_lang = st.selectbox(
